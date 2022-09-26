46ebd7f36376a6a542f0366ba51399203a68144e
Ronny Klein <eLindros@users.noreply.github.com>
2022-09-26 21:55:20 +0200

    initial

---
+++ src/components/Generic/ExternalInfoIframe.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          text
+          v-bind="attrs"
+          v-on="on"
+        >
+          <slot></slot>
+        </v-btn>
+      </template>
+
+      <v-card>
+
+   
+
+        <v-card-actions>
+				<v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+           Close
+          </v-btn>
+        </v-card-actions>
+				     <v-divider></v-divider>
+						 <iframe src="{{url}}"></iframe>
+						 
+      </v-card>
+    </v-dialog>
+  </div>
+	</template>
+	
+	<script>
+  export default {
+		props: {
+			url: {
+				type: String,
+				required: true,
+				default: 'https://www.ligainsider.de',
+			},
+		},
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+  }
+	</script>
