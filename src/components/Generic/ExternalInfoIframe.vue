d84e42d371aafe0292cb7335e75ebfbcdfc67248
Ronny Klein <ronny.klein@icloud.com>
2022-09-30 15:10:56 +0200

    fix

--- src/components/Generic/ExternalInfoIframe.vue
+++ src/components/Generic/ExternalInfoIframe.vue
@@ -7,12 +7,12 @@
         </v-btn>
       </template>
 
-      <v-card height="100%">
+      <v-card height="100vh">
         <v-card-actions>
           <v-btn color="primary" text @click="dialog = false"> Close </v-btn>
         </v-card-actions>
         <v-divider></v-divider>
-        <iframe :src="url" width="100%" height="100vh"></iframe>
+        <iframe :src="url" width="100vw" height="100vh"></iframe>
       </v-card>
     </v-dialog>
   </div>
